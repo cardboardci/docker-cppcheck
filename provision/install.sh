652184e34a51321370f07d436c904306315d7ae4
Jonathan Beverly <jrbeverly@live.com>
2020-01-03 19:29:11 -0500

    List the dependencies of the image in pkglist (#11)
    This should have been done before merging in the work for #8, but bulk changes are always fun.
    
    This PR lists all of the dependencies of the image in the pkglist file. Additionally it disables the run-on-push trigger of the versions script.

--- provision/install.sh
+++ provision/install.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
-apt-get update
-apt-get install -y build-essential libpcre3-dev curl python python-pygments
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+mkdir /cfg
 
-mkdir /cfg 
-
-cd /tmp \
-curl -L https://github.com/danmar/cppcheck/archive/${VERSION}.tar.gz | tar xz
+cd /tmp
+curl -sSL https://github.com/danmar/cppcheck/archive/${VERSION}.tar.gz | tar xz
 cd cppcheck-${VERSION}
 SRCDIR=build CFGDIR=/cfg HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" make
 SRCDIR=build CFGDIR=/cfg HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" make install
